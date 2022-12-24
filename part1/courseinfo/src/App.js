8c3bfdba6e5f7678be14e55e3f555cf2340d7b24
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2022-12-24 14:54:11 -0700

    Finish exercises 1.3 to 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,19 +3,19 @@ const App = () => {
     name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
@@ -31,7 +31,7 @@ const Header = (props) => {
     <>
       <h1>{props.course}</h1>
     </>
-  )
+  );
 };
 
 const Content = (props) => {
@@ -41,7 +41,7 @@ const Content = (props) => {
       <Part part={props.parts[1]} />
       <Part part={props.parts[2]} />
     </div>
-  )
+  );
 };
 
 const Part = (props) => {
@@ -51,15 +51,20 @@ const Part = (props) => {
         {props.part.name} {props.part.exercises}
       </p>
     </>
-  )
+  );
 };
 
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
     </>
-  )
+  );
 };
 
 export default App;
