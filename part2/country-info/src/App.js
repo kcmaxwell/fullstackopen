e526479d929100143a07ae88d91d3c696f744841
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2022-12-30 18:54:22 -0700

    Finish exercise 2.12

---
+++ part2/country-info/src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [filter, setFilter] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v2/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <div>
+        Find countries: <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <div>
+        {filteredCountries.length === 1 ? (
+          <div>
+            <h2>{filteredCountries[0].name}</h2>
+            <p>Capital: {filteredCountries[0].capital}</p>
+            <ul>
+              {filteredCountries[0].languages.map((language) => (
+                <li key={language.name}>{language.name}</li>
+              ))}
+            </ul>
+            <img src={filteredCountries[0].flag} alt="Country Flag" />
+          </div>
+        ) : filteredCountries.length > 10 ? (
+          <p>Too many matches, specify another filter</p>
+        ) : (
+          filteredCountries.map((country) => (
+            <p key={country.name}>{country.name}</p>
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
