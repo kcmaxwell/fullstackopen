3952dd5873897c61e65da7fe048249a184adf15a
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2022-12-31 14:45:32 -0700

    Finish exercise 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -24,9 +24,15 @@ const App = () => {
     } else if (checkDuplicate()) {
       alert(`${newName} already exists in the phonebook.`);
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
-      setNewName("");
-      setNewNumber("");
+      const newPerson = {
+        name: newName,
+        number: newNumber
+      }
+      axios.post('http://localhost:3001/persons', newPerson).then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName("");
+        setNewNumber("");
+      })
     }
   };
 
