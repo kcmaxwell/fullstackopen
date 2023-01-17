f7a282450a5ff938bb6b507a8ffa153baeb9c7f1
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-01-16 20:23:44 -0700

    Finish exercises 6.6 to 6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addVote } from '../reducers/anecdoteReducer';
+
+function AnecdoteList() {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+
+  const vote = (id) => {
+    console.log('vote', id);
+    dispatch(addVote(id));
+  };
+
+  const compareAnecdotes = (a, b) => {
+    if (a.votes > b.votes) { return -1; }
+    if (b.votes < a.votes) { return 1; }
+    return 0;
+  };
+
+  return (
+    <div>
+      {anecdotes.sort(compareAnecdotes).map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AnecdoteList;
