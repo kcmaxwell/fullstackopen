b2e9b58c66014c76643f5f8359b1b9920e12591f
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-01-21 23:18:54 -0700

    Finish exercise 7.1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link,
+} from 'react-router-dom';
 
 const Menu = () => {
   const padding = {
     paddingRight: 5,
   };
   return (
-    <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
-    </div>
+      <div>
+        <Link style={padding} to="/">anecdotes</Link>
+        <Link style={padding} to="/create">create new</Link>
+        <Link style={padding} to="/about">about</Link>
+      </div>
   );
 };
 
@@ -28,11 +32,15 @@ const About = () => (
     <p>According to Wikipedia:</p>
 
     <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+      Occasionally humorous, anecdotes differ from jokes because their primary purpose
+      is not simply to provoke laughter but to reveal a truth more general
+      than the brief tale itself, such as to characterize a person by delineating
+      a specific quirk or trait, to communicate an abstract idea about a
+      person, place, or thing through the concrete details of a short narrative.
+      An anecdote is &quot;a story with a point.&quot;</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+    <p>Software engineering is full of excellent anecdotes,
+       at this app you can find the best and add more.</p>
   </div>
 );
 
@@ -99,34 +107,40 @@ const App = () => {
     },
   ]);
 
-  const [notification, setNotification] = useState('');
+  // const [notification, setNotification] = useState('');
 
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000);
     setAnecdotes(anecdotes.concat(anecdote));
   };
 
-  const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
+  // const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
 
-  const vote = (id) => {
-    const anecdote = anecdoteById(id);
+  // const vote = (id) => {
+  //   const anecdote = anecdoteById(id);
 
-    const voted = {
-      ...anecdote,
-      votes: anecdote.votes + 1,
-    };
+  //   const voted = {
+  //     ...anecdote,
+  //     votes: anecdote.votes + 1,
+  //   };
 
-    setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
-  };
+  //   setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
+  // };
 
   return (
     <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
+      <Router>
+        <h1>Software anecdotes</h1>
+        <Menu />
+
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/create' element={<CreateNew addNew={addNew} />} />
+          <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 };
