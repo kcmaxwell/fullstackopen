4bd95076d083a4213b809bf5bedc8a3b06231ead
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-02-12 16:07:44 -0700

    Fix exercise 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -2,12 +2,12 @@ import express from 'express';
 const app = express();
 const cors = require('cors'); // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
 
-app.use(cors()); // eslint-disable=line @typescript-eslint/no-unsafe-call
+app.use(cors()); // eslint-disable-line @typescript-eslint/no-unsafe-call
 app.use(express.json());
 
 const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
